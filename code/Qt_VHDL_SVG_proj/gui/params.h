e5e893e79c6b1074c4a910c496c9cfaa9590938a
LiroKwid <baptisterodier@gmail.com>
2024-05-29 18:50:23 +0200

    refactor: Add TEMP_SVG_ER_PATH constant for error SVG file
    This commit adds the TEMP_SVG_ER_PATH constant to the params.h file, which specifies the path to the error SVG file used for debugging. The constant is used in the mainwindow.cpp file to load the error SVG when the corresponding button is clicked. This change improves the debugging capabilities of the application.

--- code/Qt_VHDL_SVG_proj/gui/params.h
+++ code/Qt_VHDL_SVG_proj/gui/params.h
@@ -5,6 +5,7 @@
 #define MAX_DEBUG_MESSAGES_NB 200
 
 #define TEMP_SVG_PATH  ":/svg/mux_symp.svg"
+#define TEMP_SVG_ER_PATH ":/svg/mux_symp_er.svg"
 #define TEMP_SVG_PATH2 ":/svg/mux.svg"
 #define TEMP_SVG_PATH3 ":/svg/Schematic_Paco.svg"
 
